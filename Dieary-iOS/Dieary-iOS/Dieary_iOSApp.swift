09636d4ef9c841518c7f880e68ba50478312b5c4
hsj._.06 <gtw030488@gmail.com>
2023-07-14 10:07:31 +0900

    init :: project init

---
+++ Dieary-iOS/Dieary-iOS/Dieary_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dieary_iOSApp.swift
+//  Dieary-iOS
+//
+//  Created by 홍승재 on 2023/07/14.
+//
+
+import SwiftUI
+
+@main
+struct Dieary_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
