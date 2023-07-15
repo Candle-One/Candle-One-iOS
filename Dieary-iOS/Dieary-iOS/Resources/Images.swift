5a0ea3e867ace663b295434d9fab98bddb30bb08
hsj._.06 <gtw030488@gmail.com>
2023-07-16 02:29:22 +0900

    feat :: diearyimage

--- Dieary-iOS/Dieary-iOS/Resources/Images.swift
+++ Dieary-iOS/Dieary-iOS/Resources/Images.swift
@@ -1,13 +1,46 @@
 import SwiftUI
 
-extension Image {
-    static let ambiguous: Image = Image("Ambiguous")
-    static let angry: Image = Image("Angry")
-    static let excited: Image = Image("Excited")
-    static let flutter: Image = Image("Flutter")
-    static let happy: Image = Image("Happy")
-    static let sad: Image = Image("Sad")
-    static let stuffy: Image = Image("Stuffy")
-    static let surprised: Image = Image("Surprised")
-    static let tired: Image = Image("Tired")
+import SwiftUI
+
+public struct DiearyImage: View {
+    public enum Image: String {
+        case lockFire = "LockFire"
+        case emptyFire = "EmptyFire"
+        case ambiguous = "Ambiguous"
+        case angry = "Angry"
+        case excited = "Excited"
+        case flutter = "Flutter"
+        case happy = "Happy"
+        case sad = "Sad"
+        case stuffy = "Stuffy"
+        case surprised = "Surprised"
+        case tired = "Tired"
+    }
+
+    private var image: Image
+    private var renderingMode: SwiftUI.Image.TemplateRenderingMode
+
+    public init(
+        _ image: Image,
+        renderingMode: SwiftUI.Image.TemplateRenderingMode = .original
+    ) {
+        self.image = image
+        self.renderingMode = renderingMode
+    }
+
+    public var body: some View {
+        switch image {
+        case .lockFire, .emptyFire:
+            dmsImageToImage()
+                .renderingMode(renderingMode)
+        default:
+            dmsImageToImage()
+                .resizable()
+                .renderingMode(renderingMode)
+        }
+    }
+
+    private func dmsImageToImage() -> SwiftUI.Image {
+        SwiftUI.Image(image.rawValue)
+    }
 }
