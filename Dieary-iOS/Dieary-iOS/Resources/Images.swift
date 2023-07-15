0a6935b9a07800edd19a8579e89bf4ce7b873fc1
hsj._.06 <gtw030488@gmail.com>
2023-07-16 01:04:50 +0900

    init :: face icon

---
+++ Dieary-iOS/Dieary-iOS/Resources/Images.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+extension Image {
+    static let ambiguous: Image = Image("Ambiguous")
+    static let angry: Image = Image("Angry")
+    static let excited: Image = Image("Excited")
+    static let flutter: Image = Image("Flutter")
+    static let happy: Image = Image("Happy")
+    static let sad: Image = Image("Sad")
+    static let stuffy: Image = Image("Stuffy")
+    static let surprised: Image = Image("Surprised")
+    static let tired: Image = Image("Tired")
+}
