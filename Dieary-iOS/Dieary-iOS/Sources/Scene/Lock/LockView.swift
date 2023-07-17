35a26d4497316cf066227b167bf29eec5e1eb62e
hsj._.06 <gtw030488@gmail.com>
2023-07-17 23:42:24 +0900

    feat :: lockView

--- Dieary-iOS/Dieary-iOS/Sources/Scene/Lock/LockView.swift
+++ Dieary-iOS/Dieary-iOS/Sources/Scene/Lock/LockView.swift
@@ -2,41 +2,53 @@ import SwiftUI
 
 struct LockView: View {
     @StateObject var viewModel = LockViewModel()
+    @State var xOffset: CGFloat = 0
+
     var body: some View {
         VStack(spacing: 40) {
             Spacer()
+
             Text("암호 입력")
                 .diearyFont(.title(.title1), color: .white)
+
             HStack {
                 Group {
                     ForEach(0..<viewModel.password.count, id: \.self) { _ in
                         DiearyImage(.lockFire)
                     }
+
                     ForEach(viewModel.password.count..<4, id: \.self) { _ in
                         DiearyImage(.emptyFire)
                     }
                 }
                 .frame(width: 60, height: 88)
             }
+
             Spacer()
+
             HStack(alignment: .top) {
                 VStack {
                     keyboardCell(1)
                     keyboardCell(3)
                     keyboardCell(7)
                 }
+
                 Spacer()
+
                 VStack {
                     keyboardCell(2)
                     keyboardCell(4)
                     keyboardCell(8)
                     keyboardCell(0)
                 }
+
                 Spacer()
+
                 VStack {
                     keyboardCell(3)
                     keyboardCell(6)
                     keyboardCell(9)
+
                     Button { viewModel.deleteTapped() } label: {
                         Image(systemName: "delete.backward")
                             .font(.custom("Pretendard-Medium", size: 30))
@@ -48,6 +60,11 @@ struct LockView: View {
             .padding(.horizontal, 20)
         }
         .diearyBackground()
+        .fullScreenCover(isPresented: $viewModel.isNavigateHome) {
+            NavigationView {
+                HomeView()
+            }
+        }
     }
 
     @ViewBuilder
@@ -57,11 +74,14 @@ struct LockView: View {
                 .diearyFont(.etc(.keyboardText), color: .white)
                 .frame(width: 90, height: 90)
         }
+        .disabled(viewModel.inputDisabled)
     }
 }
 
 struct LockView_Previews: PreviewProvider {
     static var previews: some View {
-        LockView()
+        NavigationView {
+            LockView()
+        }
     }
 }
--- Dieary-iOS/Dieary-iOS/Sources/Scene/Lock/LockViewModel.swift
+++ Dieary-iOS/Dieary-iOS/Sources/Scene/Lock/LockViewModel.swift
@@ -1,13 +1,33 @@
 import Foundation
+import SwiftUI
 
 final class LockViewModel: ObservableObject {
     @Published var password: [Int] = []
+    @Published var isNavigateHome = false
+    @Published var inputDisabled = false
     
     func keypadTapped(_ num: Int) {
         password.append(num)
+        if password.count >= 4 {
+            inputDisabled = true
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                withAnimation {
+                    self.navigateHomeView()
+                }
+            }
+        }
     }
 
     func deleteTapped() {
         let _ = password.popLast()
+    }
+
+    private func navigateHomeView() {
+        if password.map({ String($0) }).joined() == "0000" {
+            self.isNavigateHome.toggle()
+        } else {
+            self.password = []
+            self.inputDisabled = false
+        }
     }
 }
